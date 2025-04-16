d6237d232bf8030b9c19d1a81b986f53e559100f
laylin41 <yovich2006@gmail.com>
2025-04-16 14:57:32 +0300

    Add project files.

---
+++ Hubs/MeetingHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ZOOM_RIPOFF.Hubs
+{
+    public class MeetingHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using ZOOM_RIPOFF.Components;
+using ZOOM_RIPOFF.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<MeetingHub>("/meetinghub"); // URL для підключення
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
